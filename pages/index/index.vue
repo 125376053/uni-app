0399407d795478c15c18be2b3f781e5c32f1ee3f
zhangzhaojie <125376053@qq.com>
2019-02-27 19:04:57 +0800

    ...

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -37,7 +37,7 @@
 			<view>
 				<video
 					id="myVideo"
-					src="../../static/video/1.mp4"
+					src="http://125.39.54.27/vwecam.gtimg.com/1009_c8ff19ead79e46cbb30f664b6a8569c5.f20.mp4?sha=03CA8F336D1A4ECBCFC77A52D42B7C2021B9C23C&ptype=http&vkey=FC98FF384323BA1C056E81C81FA968AB427107F7F4DE372FE0CAA181B1830B163FD4EF5F95390F3F1E5AE39385DAC05588390B5A3442C061&sdtfrom=v1000&owner=0&ocid=479466156&ocid=424744364&ocid=203017226"
 					enable-danmu
 					danmu-btn
 					controls
