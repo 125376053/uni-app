a66945975616b5f050d372c12b6529759f0c30ec
zhangzhaojie <125376053@qq.com>
2019-02-27 19:29:33 +0800

    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,13 +7,7 @@
 		<view class="uni-padding-wrap">
 			<view class="page-section">
 				<view class="page-section-spacing">
-					<swiper
-						class="swiper"
-						:indicator-dots="indicatorDots"
-						:autoplay="autoplay"
-						:interval="interval"
-						:duration="duration"
-					>
+					<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
 						<swiper-item>
 							<view class="swiper-item">
 								<image src="../../static/image/go2.jpg"></image>
@@ -35,104 +29,107 @@
 		</ul>
 		<view class="uni-padding-wrap uni-common-mt">
 			<view>
-				<video
-					id="myVideo"
-					src="http://125.39.54.27/vwecam.gtimg.com/1009_c8ff19ead79e46cbb30f664b6a8569c5.f20.mp4?sha=03CA8F336D1A4ECBCFC77A52D42B7C2021B9C23C&ptype=http&vkey=FC98FF384323BA1C056E81C81FA968AB427107F7F4DE372FE0CAA181B1830B163FD4EF5F95390F3F1E5AE39385DAC05588390B5A3442C061&sdtfrom=v1000&owner=0&ocid=479466156&ocid=424744364&ocid=203017226"
-					enable-danmu
-					danmu-btn
-					controls
-				></video>
+				<video id="myVideo" src="http://125.39.54.27/vwecam.gtimg.com/1009_c8ff19ead79e46cbb30f664b6a8569c5.f20.mp4?sha=03CA8F336D1A4ECBCFC77A52D42B7C2021B9C23C&ptype=http&vkey=FC98FF384323BA1C056E81C81FA968AB427107F7F4DE372FE0CAA181B1830B163FD4EF5F95390F3F1E5AE39385DAC05588390B5A3442C061&sdtfrom=v1000&owner=0&ocid=479466156&ocid=424744364&ocid=203017226"
+				 enable-danmu danmu-btn controls></video>
 			</view>
 		</view>
+		
 	</view>
 </template>
 
 <script>
-// import { uniList, uniListItem } from '@dcloudio/uni-ui';
-export default {
-	components: {
-		//uniList,
-		//uniListItem
-	},
-	data() {
-		return {
-			indicatorDots: true,
-			autoplay: true,
-			interval: 2000,
-			duration: 500
-		};
-	},
-	computed: {
-		userInfo() {
-			return this.$store.getters.userInfo;
-		}
-	},
-	methods: {
-		gotoVideo(){
-			wx.redirectTo({
-				url: '/pages/video/video'
-			})
-		}
-	},
-	watch: {
-		userInfo(a) {
-			console.log(a);
-			if (a) {
-				uni.setNavigationBarTitle({
-					title: '首页'
-				});
+	// import { uniList, uniListItem } from '@dcloudio/uni-ui';
+	export default {
+		components: {
+			//uniList,
+			//uniListItem
+		},
+		data() {
+			return {
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500,
+				
+			};
+		},
+		computed: {
+			userInfo() {
+				return this.$store.getters.userInfo;
 			}
-		}
-	},
-	onLoad() {
-		console.log(123);
-		uni.showLoading({
-			title: '加载中'
-		});
-		setTimeout(() => {
-			uni.hideLoading();
-		}, 2000);
-	},
-	onReady() {}
-};
+		},
+		methods: {
+			gotoVideo() {
+				wx.redirectTo({
+					url: '/pages/video/video'
+				})
+			}
+		},
+		watch: {
+			userInfo(a) {
+				console.log(a);
+				if (a) {
+					uni.setNavigationBarTitle({
+						title: '首页'
+					});
+				}
+			}
+		},
+		onLoad() {
+			console.log(123);
+			uni.showLoading({
+				title: '加载中'
+			});
+			setTimeout(() => {
+				uni.hideLoading();
+			}, 2000);
+		},
+		onReady() {}
+	};
 </script>
 
 <style>
-.top {
-	height: 40px;
-	line-height: 40px;
-	display: flex;
-	justify-content: center;
-	align-content: center;
-}
-.avtar {
-	width: 40px;
-	height: 40px;
-	border-radius: 50%;
-	display: block;
-	margin-left: 10px;
-}
-.uni-padding-wrap {
-	margin: 10px;
-}
-.swiper {
-	height: 169px;
-}
-.swiper-item image {
-	width: 100%;
-	height: 169px;
-}
-.uni-common-mt video{
-	width: 100%;
-}
+	.top {
+		height: 40px;
+		line-height: 40px;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+	}
 
-ul{
-	padding: 10px;
-}
-li{
-	height: 30px;
-	line-height: 30px;
-	border-bottom: 1px solid #ccc;
-	font-size:14px;
-}
+	.avtar {
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+		display: block;
+		margin-left: 10px;
+	}
+
+	.uni-padding-wrap {
+		margin: 10px;
+	}
+
+	.swiper {
+		height: 169px;
+	}
+
+	.swiper-item image {
+		width: 100%;
+		height: 169px;
+	}
+
+	.uni-common-mt video {
+		width: 100%;
+	}
+
+	ul {
+		padding: 10px;
+	}
+
+	li {
+		height: 30px;
+		line-height: 30px;
+		border-bottom: 1px solid #ccc;
+		font-size: 14px;
+	}
 </style>
