db59f4324078e1fe6a006081be4b5bf130b0b5df
zhangzhaojie <125376053@qq.com>
2019-02-28 17:15:33 +0800

    tab不渲染

---
+++ pages/adress/adress.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<button @click="getAdress">获取地址信息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			getAdress() {
+				wx.chooseAddress({
+					success(res) {
+						console.log(res.userName)
+						console.log(res.postalCode)
+						console.log(res.provinceName)
+						console.log(res.cityName)
+						console.log(res.countyName)
+						console.log(res.detailInfo)
+						console.log(res.nationalCode)
+						console.log(res.telNumber)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
